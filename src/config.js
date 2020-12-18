3f1a11f390df35c9107e87727a564d59d240451a
Seth Hall <sethburtonhall@gmail.com>
2020-12-18 00:03:41 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+  s3: {
+    REGION: 'us-east-1',
+    BUCKET: 'notes-image-upload'
+  },
+  apiGateway: {
+    REGION: 'us-east-1',
+    URL: 'https://0f7jby961h.execute-api.us-east-1.amazonaws.com/prod'
+  },
+  cognito: {
+    REGION: 'us-east-1',
+    USER_POOL_ID: 'us-east-1_up5LyaZGS',
+    APP_CLIENT_ID: '6e4hpfv9vrgj187mpq6k479kcq',
+    IDENTITY_POOL_ID: 'us-east-1:2bb2416f-9538-465c-9cd6-4923c6f47f22'
+  }
+};
+
+export default config;
